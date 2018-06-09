b440cf5881524abd70483eef587e4c9675746a6d
hwang777 <hw2@hotmail.com>
2018-06-09 15:07:26 -0500

    first

---
+++ src/ttt/Main.java
@@ -0,0 +1,19 @@
+package ttt;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+		
+		@Override
+		public void run() {
+			new TicTacToeGUI();
+		}
+		
+		});
+		
+	}
+
+}
